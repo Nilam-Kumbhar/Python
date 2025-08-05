b15520ebfd94cd45de70a534a36bf0897e0c5101
Nilam-Kumbhar <nilamkumbhar1605@gmail.com>
2025-08-05 19:30:19 +0530

    python program

--- pattern5.py
+++ pattern5.py
@@ -1,4 +1,4 @@
-n = int(input("Enter value of n "))
+n = int(input("Enter value of n="))
 for i in range(n, 0, -1):            
     for j in range(65, 65 + i):      
         print(chr(j), end=' ')
