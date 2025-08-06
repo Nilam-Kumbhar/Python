0a3a2434335bae26e72decb391541e5f75511d21
Nilam-Kumbhar <nilamkumbhar1605@gmail.com>
2025-08-06 14:23:23 +0530

    added more oprations

--- stringoperations.py
+++ stringoperations.py
@@ -6,3 +6,10 @@ print(str.lower())
 print(str[1])
 str1="world"
 print(str + str1)
+print(len(str))
+print(str.title())
+print(str.replace("HELLO","hiii"))
+print(str.find("O"))
+
+s=["nilam","shruti"]
+print(",".join(s))
