fd33ff3ea89c38999d7d2fd81c003bd391acd2f5
Nilam-Kumbhar <nilamkumbhar1605@gmail.com>
2025-08-06 15:09:34 +0530

    new program

---
+++ set.py
@@ -0,0 +1,20 @@
+s={10,20,30,40}
+print("set:",s)
+
+s.pop()
+print("popped",s.pop())
+print("set after pop:",s)
+
+s.add(3)
+print("set after adding 3:",s)
+
+s.update([50,60])
+print("updating 50 and 60:",s)
+
+s.clear()
+print("clear set:",s)
+
+a = {1, 2, 3}
+b = {2, 3, 4}
+print(a.difference(b)) 
+print(b.difference(a)) 
